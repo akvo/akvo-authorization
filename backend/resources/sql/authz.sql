20693e2f16280bdded5cefb8c43904cd805e30ff
Daniel Lebrero Berna <dlebrero@gmail.com>
2019-06-18 21:31:52 +0200

    Authz DB call returns minimum data
    Improves the performance when the user has a lot of surveys (>10k) from
    ~1 sec to ~300ms

--- backend/resources/sql/authz.sql
+++ backend/resources/sql/authz.sql
@@ -12,4 +12,4 @@ WITH
                 and ufi.user_id = :user-id
                 and ufi.super_admin = true
                 and n.flow_id = 0)
-select * from nodes n WHERE n.type = 'SURVEY' AND n.full_path <@ ARRAY(select * FROM PERMS)
+select flow_id, flow_instance from nodes n WHERE n.type = 'SURVEY' AND n.full_path <@ ARRAY(select * FROM PERMS)
