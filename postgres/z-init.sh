ec90f367d46eb7a63c1a766f28fe2e75746d6433
Daniel Lebrero Berna <dlebrero@gmail.com>
2019-06-19 10:54:50 +0200

    Creating a unilog db in local dev

--- postgres/z-init.sh
+++ postgres/z-init.sh
@@ -2,6 +2,14 @@
 
 set -eu
 
+## Unilog event database
+psql -c "CREATE ROLE uniloguser WITH PASSWORD 'uniloguserpassword' CREATEDB LOGIN;"
+psql -c "CREATE DATABASE u_unilog_events WITH OWNER = uniloguser TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';"
+
+psql -d u_unilog_events -U uniloguser -c "CREATE TABLE IF NOT EXISTS event_log (id BIGSERIAL PRIMARY KEY, payload JSONB UNIQUE);"
+psql -d u_unilog_events -U uniloguser -c "CREATE INDEX timestamp_idx ON event_log(cast(payload->'context'->>'timestamp' AS numeric));"
+
+## Authz service database
 psql -c "CREATE ROLE authzuser WITH PASSWORD 'authzpasswd' CREATEDB LOGIN;"
 
 psql -c "CREATE DATABASE authzdb WITH OWNER = authzuser TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';"
