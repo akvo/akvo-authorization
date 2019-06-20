edab217a8808f5394354a4f95340321c38b4cbbb
Daniel Lebrero Berna <dlebrero@gmail.com>
2019-06-20 13:23:43 +0200

    Fix nrepl halt signature

--- backend/src/akvo_authorization/util/nrepl.clj
+++ backend/src/akvo_authorization/util/nrepl.clj
@@ -5,5 +5,5 @@
 (defmethod ig/init-key ::nrepl [_ {:keys [bind port]}]
   (start-server :bind bind :port port))
 
-(defmethod ig/halt-key! ::nrepl [server]
+(defmethod ig/halt-key! ::nrepl [_ server]
   (stop-server server))
