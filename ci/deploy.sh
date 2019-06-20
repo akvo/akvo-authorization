0507295b3932e1c6712e86d9a610df241791ac39
Daniel Lebrero Berna <dlebrero@gmail.com>
2019-06-20 17:52:27 +0200

    Forgot to push the nginx image

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -57,6 +57,7 @@ else
     log Pushing images
     gcloud auth configure-docker
     docker push eu.gcr.io/${PROJECT_NAME}/akvo-authz
+    docker push eu.gcr.io/${PROJECT_NAME}/akvo-authz-nginx
     docker push eu.gcr.io/${PROJECT_NAME}/akvo-authz-nginx-auth0
 fi
 
