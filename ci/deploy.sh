cf5f1b366cce73af7acfde576904cf7e37bac133
Daniel Lebrero Berna <dlebrero@gmail.com>
2019-06-21 11:14:03 +0200

    Split consumer from api in k8s

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -34,25 +34,35 @@ if [[ "${TRAVIS_TAG:-}" =~ promote-.* ]]; then
     log Environment is production
     gcloud container clusters get-credentials production
     ENVIRONMENT=production
-    POD_CPU_REQUESTS="2000m"
-    POD_CPU_LIMITS="4000m"
-    POD_MEM_REQUESTS="800Mi"
-    POD_MEM_LIMITS="800Mi"
+    POD_CPU_REQUESTS="400m"
+    POD_CPU_LIMITS="2000m"
+    POD_MEM_REQUESTS="400Mi"
+    POD_MEM_LIMITS="400Mi"
     PROXY_POD_CPU_REQUESTS="100m"
     PROXY_POD_CPU_LIMITS="200m"
     PROXY_POD_MEM_REQUESTS="16Mi"
     PROXY_POD_MEM_LIMITS="32Mi"
+
+    UNILOG_CONSUMER_POD_CPU_REQUESTS="500m"
+    UNILOG_CONSUMER_POD_CPU_LIMITS="2000m"
+    UNILOG_CONSUMER_POD_MEM_REQUESTS="800Mi"
+    UNILOG_CONSUMER_POD_MEM_LIMITS="800Mi"
 else
     log Environment is test
     gcloud container clusters get-credentials test
-    POD_CPU_REQUESTS="2000m"
-    POD_CPU_LIMITS="4000m"
-    POD_MEM_REQUESTS="800Mi"
-    POD_MEM_LIMITS="800Mi"
+    POD_CPU_REQUESTS="200m"
+    POD_CPU_LIMITS="1000m"
+    POD_MEM_REQUESTS="400Mi"
+    POD_MEM_LIMITS="400Mi"
     PROXY_POD_CPU_REQUESTS="50m"
     PROXY_POD_CPU_LIMITS="100m"
     PROXY_POD_MEM_REQUESTS="16Mi"
     PROXY_POD_MEM_LIMITS="32Mi"
+
+    UNILOG_CONSUMER_POD_CPU_REQUESTS="500m"
+    UNILOG_CONSUMER_POD_CPU_LIMITS="1000m"
+    UNILOG_CONSUMER_POD_MEM_REQUESTS="800Mi"
+    UNILOG_CONSUMER_POD_MEM_LIMITS="800Mi"
 
     log Pushing images
     gcloud auth configure-docker
@@ -73,9 +83,18 @@ sed -e "s/\$TRAVIS_COMMIT/$TRAVIS_COMMIT/" \
   -e "s/\${PROXY_POD_MEM_REQUESTS}/${PROXY_POD_MEM_REQUESTS}/" \
   -e "s/\${PROXY_POD_CPU_LIMITS}/${PROXY_POD_CPU_LIMITS}/" \
   -e "s/\${PROXY_POD_MEM_LIMITS}/${PROXY_POD_MEM_LIMITS}/" \
-  ci/k8s/akvo-unilog.yaml > final-akvo-authz.yaml
+  ci/k8s/akvo-authz-api.yaml > final-akvo-authz.yaml
+
+sed -e "s/\$TRAVIS_COMMIT/$TRAVIS_COMMIT/" \
+  -e "s/\${ENVIRONMENT}/${ENVIRONMENT}/" \
+  -e "s/\${UNILOG_CONSUMER_POD_CPU_REQUESTS}/${POD_CPU_REQUESTS}/" \
+  -e "s/\${UNILOG_CONSUMER_POD_MEM_REQUESTS}/${POD_MEM_REQUESTS}/" \
+  -e "s/\${UNILOG_CONSUMER_POD_CPU_LIMITS}/${POD_CPU_LIMITS}/" \
+  -e "s/\${UNILOG_CONSUMER_POD_MEM_LIMITS}/${POD_MEM_LIMITS}/" \
+  ci/k8s/akvo-authz-unilog-consumer.yaml > final-akvo-authz-unilog-consumer.yaml
 
 kubectl apply -f final-akvo-authz.yaml
+kubectl apply -f final-akvo-authz-unilog-consumer.yaml
 kubectl apply -f ci/k8s/service.yaml
 
 ci/k8s/wait-for-k8s-deployment-to-be-ready.sh
